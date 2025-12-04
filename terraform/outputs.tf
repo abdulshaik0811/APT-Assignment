16cc626e4f90c0ad6ff0775599ffdf50081e0cf3
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 21:35:48 +0530

    Format outputs.tf for better readability

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,4 +1,15 @@
-output "alb_dns_name" { value = aws_lb.alb.dns_name }
-output "asg_name" { value = aws_autoscaling_group.asg.name }
-output "vpc_id" { value = aws_vpc.main.id }
-output "private_subnets" { value = [for s in aws_subnet.private : s.id] }
+output "alb_dns_name" {
+  value = aws_lb.alb.dns_name
+}
+
+output "asg_name" {
+  value = aws_autoscaling_group.asg.name
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
+}
+
+output "private_subnets" {
+  value = [for s in aws_subnet.private : s.id]
+}
