5614ada1083d6c3f1067148f009b2a0371a4a1a0
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 21:35:08 +0530

    Update networking.tf

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -6,6 +6,25 @@ resource "aws_internet_gateway" "igw" {
   }
 }
 
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "oneclick-nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public[0].id
+  depends_on    = [aws_internet_gateway.igw]
+
+  tags = {
+    Name = "oneclick-nat"
+  }
+}
+
+# PUBLIC ROUTE
 resource "aws_route_table" "public_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -20,11 +39,12 @@ resource "aws_route_table" "public_rt" {
 }
 
 resource "aws_route_table_association" "public_assoc" {
-  count          = length(aws_subnet.public)
-  subnet_id      = aws_subnet.public[count.index].id
+  count = length(aws_subnet.public)
+  subnet_id = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public_rt.id
 }
 
+# PRIVATE ROUTE
 resource "aws_route_table" "private_rt" {
   vpc_id = aws_vpc.main.id
 
@@ -39,7 +59,7 @@ resource "aws_route_table" "private_rt" {
 }
 
 resource "aws_route_table_association" "private_assoc" {
-  count          = length(aws_subnet.private)
-  subnet_id      = aws_subnet.private[count.index].id
+  count = length(aws_subnet.private)
+  subnet_id = aws_subnet.private[count.index].id
   route_table_id = aws_route_table.private_rt.id
 }
