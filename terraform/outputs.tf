2c3cb4a9174b3fbb85a567c902e964d2e62b33f2
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 22:27:07 +0530

    Add outputs for VPC, ALB, and security groups

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,20 +1,44 @@
-############################################
-# OUTPUT: ALB DNS NAME
-############################################
-output "alb_dns_name" {
-  value = aws_lb.alb.dns_name
-}
-
-############################################
-# OUTPUT: VPC ID
-############################################
 output "vpc_id" {
-  value = aws_vpc.main.id
+  description = "VPC ID"
+  value       = aws_vpc.main.id
 }
 
-############################################
-# OUTPUT: PRIVATE SUBNET IDS
-############################################
-output "private_subnets" {
-  value = [for s in aws_subnet.private : s.id]
+output "alb_dns_name" {
+  description = "DNS name of the ALB"
+  value       = aws_lb.main.dns_name
+}
+
+output "alb_url" {
+  description = "URL of the ALB"
+  value       = "http://${aws_lb.main.dns_name}"
+}
+
+output "target_group_arn" {
+  description = "ARN of the target group"
+  value       = aws_lb_target_group.app.arn
+}
+
+output "asg_name" {
+  description = "Name of the Auto Scaling Group"
+  value       = aws_autoscaling_group.app.name
+}
+
+output "ec2_security_group_id" {
+  description = "EC2 Security Group ID"
+  value       = aws_security_group.ec2.id
+}
+
+output "alb_security_group_id" {
+  description = "ALB Security Group ID"
+  value       = aws_security_group.alb.id
+}
+
+output "private_subnet_ids" {
+  description = "IDs of private subnets"
+  value       = aws_subnet.private[*].id
+}
+
+output "public_subnet_ids" {
+  description = "IDs of public subnets"
+  value       = aws_subnet.public[*].id
 }
