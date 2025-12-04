802fa8a0fbc8551ad99ead6274084491b8c6c3db
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 21:21:07 +0530

    Create outputs.tf

---
+++ terraform/outputs.tf
@@ -0,0 +1,4 @@
+output "alb_dns_name" { value = aws_lb.alb.dns_name }
+output "asg_name" { value = aws_autoscaling_group.asg.name }
+output "vpc_id" { value = aws_vpc.main.id }
+output "private_subnets" { value = [for s in aws_subnet.private : s.id] }
