f50cf5d2fca2e36e45ff01b0f7b99cc22f2dffa7
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 22:39:03 +0530

    Create lauch_template.tf

---
+++ terraform/lauch_template.tf
@@ -0,0 +1,73 @@
+# Get latest Amazon Linux 2 AMI
+data "aws_ami" "amazon_linux_2" {
+  most_recent = true
+  owners      = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# User data script
+data "template_file" "user_data" {
+  template = file("${path.module}/user-data.sh")
+
+  vars = {
+    app_port = var.app_port
+  }
+}
+
+# Launch Template
+resource "aws_launch_template" "app" {
+  name_prefix   = "${var.project_name}-"
+  image_id      = data.aws_ami.amazon_linux_2.id
+  instance_type = var.instance_type
+
+  # No SSH key needed for SSM
+  key_name = ""
+
+  # Network configuration
+  network_interfaces {
+    associate_public_ip_address = false
+    security_groups             = [aws_security_group.ec2.id]
+  }
+
+  # IAM instance profile
+  iam_instance_profile {
+    name = aws_iam_instance_profile.ec2_profile.name
+  }
+
+  # Base64 encoded user data
+  user_data = base64encode(data.template_file.user_data.rendered)
+
+  # Tag specifications
+  tag_specifications {
+    resource_type = "instance"
+
+    tags = {
+      Name = "${var.project_name}-instance"
+    }
+  }
+
+  tag_specifications {
+    resource_type = "volume"
+
+    tags = {
+      Name = "${var.project_name}-volume"
+    }
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = {
+    Name = "${var.project_name}-launch-template"
+  }
+}
