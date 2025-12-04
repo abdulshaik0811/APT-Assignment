684881318c470d62e19beeb05d24a048fce3f85d
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 21:30:33 +0530

    Refactor IAM policy document and role configuration

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -1,23 +1,46 @@
 data "aws_iam_policy_document" "ec2_assume" {
   statement {
     actions = ["sts:AssumeRole"]
-    principals { type = "Service"; identifiers = ["ec2.amazonaws.com"] }
+
+    principals {
+      type        = "Service"
+      identifiers = ["ec2.amazonaws.com"]
+    }
   }
 }
 
 resource "aws_iam_role" "ec2_role" {
-  name = "oneclick-ec2-role"
+  name               = "oneclick-ec2-role"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
 }
 
 resource "aws_iam_role_policy" "ec2_policy" {
   name = "oneclick-ec2-inline-policy"
   role = aws_iam_role.ec2_role.id
+
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
-      { Effect = "Allow", Action = ["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents","logs:DescribeLogStreams"], Resource = "*" },
-      { Effect = "Allow", Action = ["ssm:DescribeInstanceInformation","ssm:StartSession","ssm:SendCommand","ssm:GetCommandInvocation"], Resource = "*" }
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams"
+        ],
+        Resource = "*"
+      },
+      {
+        Effect = "Allow",
+        Action = [
+          "ssm:DescribeInstanceInformation",
+          "ssm:StartSession",
+          "ssm:SendCommand",
+          "ssm:GetCommandInvocation"
+        ],
+        Resource = "*"
+      }
     ]
   })
 }
