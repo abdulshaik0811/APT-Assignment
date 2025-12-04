406b5bb3f2db6d6ebc4a9c07d26cf8befb4721f9
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 21:38:11 +0530

    Add outputs for ALB, ASG, VPC, and private subnets

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,15 +1,27 @@
+###############################
+# LOAD BALANCER OUTPUT
+###############################
 output "alb_dns_name" {
   value = aws_lb.alb.dns_name
 }
 
+###############################
+# AUTOSCALING GROUP NAME
+###############################
 output "asg_name" {
   value = aws_autoscaling_group.asg.name
 }
 
+###############################
+# VPC ID
+###############################
 output "vpc_id" {
   value = aws_vpc.main.id
 }
 
+###############################
+# PRIVATE SUBNET IDs
+###############################
 output "private_subnets" {
   value = [for s in aws_subnet.private : s.id]
 }
