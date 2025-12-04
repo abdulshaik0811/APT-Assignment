ef432fd98b3ca7b6b8bcad052143d56f2e174be0
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-04 21:19:00 +0530

    Create iam.tf

---
+++ terraform/iam.tf
@@ -0,0 +1,28 @@
+data "aws_iam_policy_document" "ec2_assume" {
+  statement {
+    actions = ["sts:AssumeRole"]
+    principals { type = "Service"; identifiers = ["ec2.amazonaws.com"] }
+  }
+}
+
+resource "aws_iam_role" "ec2_role" {
+  name = "oneclick-ec2-role"
+  assume_role_policy = data.aws_iam_policy_document.ec2_assume.json
+}
+
+resource "aws_iam_role_policy" "ec2_policy" {
+  name = "oneclick-ec2-inline-policy"
+  role = aws_iam_role.ec2_role.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      { Effect = "Allow", Action = ["logs:CreateLogGroup","logs:CreateLogStream","logs:PutLogEvents","logs:DescribeLogStreams"], Resource = "*" },
+      { Effect = "Allow", Action = ["ssm:DescribeInstanceInformation","ssm:StartSession","ssm:SendCommand","ssm:GetCommandInvocation"], Resource = "*" }
+    ]
+  })
+}
+
+resource "aws_iam_instance_profile" "ec2_profile" {
+  name = "oneclick-ec2-profile"
+  role = aws_iam_role.ec2_role.name
+}
