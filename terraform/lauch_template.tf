5756c42c6cc7625ef5f367fef3766d1a3b27efa6
abdulshaik0811 <abdulshaik0811@gmail.com>
2025-12-05 13:06:17 +0530

    Update lauch_template.tf

--- terraform/lauch_template.tf
+++ terraform/lauch_template.tf
@@ -1,73 +1,58 @@
-# Get latest Amazon Linux 2 AMI
-data "aws_ami" "amazon_linux_2" {
-  most_recent = true
-  owners      = ["amazon"]
-
-  filter {
-    name   = "name"
-    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
-  }
-
-  filter {
-    name   = "virtualization-type"
-    values = ["hvm"]
-  }
-}
-
-# User data script - make sure the file exists
-data "template_file" "user_data" {
-  template = file("${path.module}/user-data.sh")
-
-  vars = {
-    app_port = var.app_port  # This passes the variable to the script
-  }
-}
-
 # Launch Template
-resource "aws_launch_template" "app" {
-  name_prefix   = "${var.project_name}-"
-  image_id      = data.aws_ami.amazon_linux_2.id
+resource "aws_launch_template" "main" {
+  name_prefix   = "${var.project_name}-lt-"
+  image_id      = data.aws_ami.amazon_linux.id
   instance_type = var.instance_type
+  key_name      = var.key_name
 
-  # No SSH key needed for SSM
-  key_name = ""
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
 
-  # Network configuration
-  network_interfaces {
-    associate_public_ip_address = false
-    security_groups             = [aws_security_group.ec2.id]
-  }
-
-  # IAM instance profile
   iam_instance_profile {
     name = aws_iam_instance_profile.ec2_profile.name
   }
 
-  # Base64 encoded user data
-  user_data = base64encode(data.template_file.user_data.rendered)
+  user_data = filebase64("${path.module}/../app/user-data.sh")
 
-  # Tag specifications
   tag_specifications {
     resource_type = "instance"
-
     tags = {
       Name = "${var.project_name}-instance"
-    }
-  }
-
-  tag_specifications {
-    resource_type = "volume"
-
-    tags = {
-      Name = "${var.project_name}-volume"
     }
   }
 
   lifecycle {
     create_before_destroy = true
   }
+}
 
-  tags = {
-    Name = "${var.project_name}-launch-template"
+# Auto Scaling Group
+resource "aws_autoscaling_group" "main" {
+  name_prefix          = "${var.project_name}-asg-"
+  vpc_zone_identifier  = aws_subnet.private[*].id
+  desired_capacity     = var.desired_capacity
+  max_size             = var.max_size
+  min_size             = var.min_size
+
+  launch_template {
+    id      = aws_launch_template.main.id
+    version = "$Latest"
+  }
+
+  target_group_arns = [aws_lb_target_group.main.arn]
+
+  tag {
+    key                 = "Name"
+    value               = "${var.project_name}-asg-instance"
+    propagate_at_launch = true
+  }
+
+  tag {
+    key                 = "Project"
+    value               = var.project_name
+    propagate_at_launch = true
+  }
+
+  lifecycle {
+    create_before_destroy = true
   }
 }
